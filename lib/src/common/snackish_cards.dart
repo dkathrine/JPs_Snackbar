b86f7b5145bbfbbde27eb95030e68d108b0d813c
Kathrine <101878099+dkathrine@users.noreply.github.com>
2025-07-30 16:32:41 +0200

    created and setup multiple files for further editing

---
+++ lib/src/common/snackish_cards.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SnackishCards extends StatelessWidget {
+  const SnackishCards({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
